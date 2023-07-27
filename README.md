f111a23398068db225c93dfa0f58159b5d0eda29
b <28288268+brianej@users.noreply.github.com>
2023-07-27 17:17:08 +0700

    1 Run

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# BlackJack_Python
- A simplified game of blackjack made by using Python
+# BlackJack Using Python
+
+This programm is a blackjack game made by using Python. The blackjack used for this programm is very simple and doesn't include stuffs like splitting pairs, doubling down or insurance. For anyone who doesn't know about the blackjack the following few paragraphs will be a short introduction to it(from https://bicyclecards.com/how-to-play/blackjack/).
+
+### The Pack
+
+The standard 52-card pack is used, but in most casinos several decks of cards are shuffled together. The six-deck game (312 cards) is the most popular. In addition, the dealer uses a blank plastic card, which is never dealt, but is placed toward the bottom of the pack to indicate when it will be time for the cards to be reshuffled. When four or more decks are used, they are dealt from a shoe (a box that allows the dealer to remove cards one at a time, face down, without actually holding one or more packs).
+
+### Objective of the Game
+
+Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.
+
+### Card Values/scoring
+
+It is up to each individual player if an ace is worth 1 or 11. Face cards are 10 and any other card is its pip value.
+
+### Betting
+
+Before the deal begins, each player places a bet, in chips, in front of them in the designated area. Minimum and maximum limits are established on the betting.
+
+### The Shuffle and Cut
+
+The dealer thoroughly shuffles portions of the pack until all the cards have been mixed and combined. 
+
+### The Deal
+
+When all the players have placed their bets, the dealer gives one card face up to each player in rotation clockwise, and then one card face up to themselves. Another round of cards is then dealt face up to each player, but the dealer takes the second card face down. Thus, each player except the dealer receives two cards face up, and the dealer receives one card face up and one card face down. (In some games, played with only one deck, the players' cards are dealt face down and they get to hold them. Today, however, virtually all Blackjack games feature the players' cards dealt face up on the condition that no player may touch any cards.)
+
+### Naturals
+
+If a player's first two cards are an ace and a "ten-card" (a picture card or 10), giving a count of 21 in two cards, this is a natural or "blackjack." If any player has a natural and the dealer does not, the dealer immediately pays that player one and a half times the amount of their bet. If the dealer has a natural, they immediately collect the bets of all players who do not have naturals, (but no additional amount). If the dealer and another player both have naturals, the bet of that player is a stand-off (a tie), and the player takes back his chips.
+
+If the dealer's face-up card is a ten-card or an ace, they look at their face-down card to see if the two cards make a natural. If the face-up card is not a ten-card or an ace, they do not look at the face-down card until it is the dealer's turn to play.
+
+### The Play
+
+The player to the left goes first and must decide whether to "stand" (not ask for another card) or "hit" (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly). Thus, a player may stand on the two cards originally dealt to them, or they may ask the dealer for additional cards, one at a time, until deciding to stand on the total (if it is 21 or under), or goes "bust" (if it is over 21). In the latter case, the player loses and the dealer collects the bet wagered. The dealer then turns to the next player to their left and serves them in the same manner.
+
+The combination of an ace with a card other than a ten-card is known as a "soft hand," because the player can count the ace as a 1 or 11, and either draw cards or not. For example with a "soft 17" (an ace and a 6), the total is 7 or 17. While a count of 17 is a good hand, the player may wish to draw for a higher total. If the draw creates a bust hand by counting the ace as an 11, the player simply counts the ace as a 1 and continues playing by standing or "hitting" (asking the dealer for additional cards, one at a time).
+
+### The Dealer's Play
+
+When the dealer has served every player, the dealers face-down card is turned up. If the total is 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand. The dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or more cards.
+
+### Signaling Intentions
+
+When a player's turn comes, they can say "Hit" or can signal for a card by scratching the table with a finger or two in a motion toward themselves, or they can wave their hand in the same motion that would say to someone "Come here!" When the player decides to stand, they can say "Stand" or "No more," or can signal this intention by moving their hand sideways, palm down and just above the table.
+
+### Settlement
+
+A bet once paid and collected is never returned. Thus, one key advantage to the dealer is that the player goes first. If the player goes bust, they have already lost their wager, even if the dealer goes bust as well. If the dealer goes over 21, the dealer pays each player who has stood the amount of that player's bet. If the dealer stands at 21 or less, the dealer pays the bet of any player having a higher total (not exceeding 21) and collects the bet of any player having a lower total. If there is a stand-off (a player having the same total as the dealer), no chips are paid out or collected.
---
+++ blackjack_python.py
@@ -0,0 +1,266 @@
+
+import random
+
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':True}
+
+class Human:
+    
+    def __init__(self):
+        self.cards = []
+
+    def hold(self,card):
+        self.cards.append(card)
+    
+    def count_cards(self):
+
+        def two_int_val(one,two,total):
+            val_one = one + total
+            val_two = two + total
+
+            if (21 - val_one > 0) and (21 - val_two > 0):
+                if val_one > val_two:
+                    return val_two
+                else:
+                    return val_one
+            elif (21 - val_one > 0) and (21 - val_two < 0):
+                return val_two
+            elif (21 - val_one < 0) and (21 - val_two > 0):
+                return val_one
+            elif (21 - val_one < 0) and (21 - val_two < 0):
+                if val_one > val_two:
+                    return val_one
+                else:
+                    return val_two
+
+        self.theres_ace = False
+        self.num_of_aces = 0
+        self.total_value = 0
+
+        for card in self.cards:
+            if (card.rank == 'Ace'):
+                self.theres_ace = True
+                self.num_of_aces += 1
+                continue
+
+            self.total_value += values[card.rank]
+
+        while self.theres_ace:
+            if (self.num_of_aces == 1):
+                return two_int_val(1,11,self.total_value)
+            elif (self.num_of_aces == 2):
+                return two_int_val(2,12,self.total_value)
+            elif (self.num_of_aces == 3):
+                return two_int_val(3,13,self.total_value)
+            elif (self.num_of_aces == 4):
+                return two_int_val(4,14,self.total_value)
+        
+        return self.total_value
+         
+
+class Player(Human):
+    
+    def __init__(self,name):
+        super().__init__()
+        self.name = name
+        self.cash = 500
+
+    def count_cards(self):
+        self.theres_ace = False
+        self.num_of_aces = 0
+        self.total_value = 0
+
+        for card in self.cards:
+            if (card.rank == 'Ace'):
+                self.theres_ace = True
+                self.num_of_aces += 1
+                continue
+
+            self.total_value += values[card.rank]
+        
+        while self.theres_ace:
+            self.val_aces = int(input(f"You hold {self.num_of_aces} aces. Pick the total values you'd like to have for the aces/s."))
+            if (self.num_of_aces == 1):
+                if self.val_aces in [1,11]:
+                    self.total_value += self.val_aces
+                    self.theres_ace = False
+                else:
+                    print("You have selected the wrong total value. Try again.")
+            elif (self.num_of_aces == 2):
+                if self.val_aces in [2,12]:
+                    self.total_value += self.val_aces
+                    self.theres_ace = False
+                else:
+                    print("You have selected the wrong total value. Try again.")
+            elif (self.num_of_aces == 3):
+                if self.val_aces in [3,13]:
+                    self.total_value += self.val_aces
+                    self.theres_ace = False
+                else:
+                    print("You have selected the wrong total value. Try again.")
+            elif (self.num_of_aces == 4):
+                if self.val_aces in [4,14]:
+                    self.total_value += self.val_aces
+                    self.theres_ace = False
+                else:
+                    print("You have selected the wrong total value. Try again.")
+        
+        print(f"The total value for your cards are {self.total_value}.")
+        
+        return self.total_value
+    
+    def __str__(self):
+        print(f"I,{self.name} have ${self.cash}!")
+    
+
+class Dealer(Human):
+    
+    def d_speak(wl,player,value):
+        if wl:
+            print(f"You,{player} have won ${value}.")
+        else:
+            print(f"You,{player} have lost ${value}.")
+
+
+class Card:
+    
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+        
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+    
+
+class Deck:
+    
+    def __init__(self):
+        self.all_cards = [] 
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit,rank))
+                
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop() 
+
+
+round_num = 0
+game_status = True
+
+while game_status:
+
+    round_num+= 1
+
+    if (round_num == 1):
+        p1_name = input("Good Day. What is your name?")
+        p1 = Player(p1_name)
+        dealer = Dealer()
+        print(f"Good Day {p1_name}. We shall now start the game of blackjack.")
+        print("You will start with $500 and when the payout for this game is 3/2. So you will win $3 for every $2 you bet.")
+        print("Let us start the game now. Have fun playing.")
+    
+    print("You have $", p1.cash)
+    pot = int(input("How much money would you like to bet?"))
+    while pot > p1.cash:
+        ans = input("You cannot bet this much money here as you don't have enough money. Press 'Y' to bet a lower amount or 'N' to exit the game.")
+        if ans == 'Y':
+            continue
+        elif ans == 'N':
+            game_status = False
+            break
+        else:
+            print("You entered a wrong amount please try again.")
+        
+    
+    deck = Deck()
+    deck.shuffle()
+
+    for i in range(4):
+        if i == 3:
+            dealer.cards.append(deck.deal_one)
+            continue
+
+        if i == 2:
+            d = deck.deal_one
+            dealer.cards.append(c)
+            print("Card for dealer:",d)
+            continue
+
+        c = deck.deal_one
+        p1.cards.append(c)
+        print("Card for player:",c)
+
+    game_not_end = True
+
+    while True:
+        p1.count_cards()
+        ans1 = input("Press 'Y' to hit or 'N' to stand.")        
+        
+        if ans1 == 'Y':
+            c = deck.deal_one
+            p1.cards.append(c)
+            print("Card for player:",c)
+            p1.count_cards()
+
+            if p1.total_value > 21:
+                print("Player has gone over 21. Player has lost.")
+                dealer.d_speak(False,p1,pot)
+                p1.cash -= pot
+                print(p1)
+                game_not_end = False
+                break
+        elif ans1 == 'N':
+            break
+        else:
+            print("You entered a wrong amount please try again.")
+    
+    print(dealer.cards[1])
+
+    while game_not_end:
+        dealer.count_cards()
+        if dealer.total_value > 21:
+                print("Dealer has gone over 21. Dealer has lost.")
+                dealer.d_speak(True,p1,pot)
+                p1.cash += pot
+                print(p1)   
+                game_not_end = False
+                break    
+        
+        if dealer.total_value  <= p1.total_value:
+            c = deck.deal_one
+            dealer.cards.append(c)
+            print("Card for player:",c)
+            dealer.count_cards()
+
+        else:
+            print("Dealer is closer to 21 than Player. Dealer has won")
+            dealer.d_speak(False,p1,pot)
+            p1.cash -= pot
+            print(p1)
+            game_not_end = False
+    
+    while True:
+        ans2 = input("Press 'Y' to keep on playing and 'N' to stop playing this game")
+
+        if ans2 == 'Y':
+            break
+        elif ans1 == 'N':
+            game_status = False
+            break
+        else:
+            print("You entered a different thing than asked. Please try again.")
+        
+    
+
+
+
+
+
+
